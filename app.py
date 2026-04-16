ef9fa59aae682f8e5f1f7f8f06fa221decc1e03f
Madhukar Eppalapelly <madhukar.eppalapelly@gmail.com>
2026-04-15 21:25:17 -0400

    final commit

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ QUESTIONS_AND_ANSWERS = {
 }
 
 CREATOR_INFO = {
-    "first_name": "Harika",
-    "last_name": "Pinninti",
-    "email": "pinninha@mail.uc.edu"
+    "first_name": "Madhukar",
+    "last_name": "Eppalapelly",
+    "email": "eppalamr@mail.uc.edu"
 }
 
 @app.route("/", methods=["GET", "POST"])
